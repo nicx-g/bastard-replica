12cd4aec0037d2632b2c91e581ae9b5100c874d3
Nicolás Gomez <nicx_g@hotmail.com>
2020-09-23 18:09:27 -0300

    Mira nico, sacaste bootstrap reboot, actualizaste el fontawesome a su version mas actual, la 5.14, mandaste al choto a owl carousel, estas creando el contador de clientes satisfechos, sacaste un par de propiedades de scss y creo que nada mas

--- js/main.js
+++ js/main.js
@@ -41,23 +41,3 @@ window.onload = function(){
 
     });
 }
-
-$(document).ready(function() {
-    $('.owl-carousel').owlCarousel({
-        center: true,
-        loop:true,
-        margin:10,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:3
-            }
-        }
-    });
-});
