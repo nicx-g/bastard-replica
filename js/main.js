fac69bddda48d5c60b772fee35a0c723737a4d4e
Nicolás Gomez <nicx_g@hotmail.com>
2020-10-02 17:04:06 -0300

    mas perfomance

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* jshint esversion: 6 */
 //GO TOP BUTTON Y MENU 
 
 window.onscroll = function(){
@@ -17,7 +18,7 @@ window.onscroll = function(){
         document.querySelector('.menu-header')
         .classList.remove('menu-header-sticky');
     }
-}
+};
 
 //GO TOP BUTTON
 
@@ -31,63 +32,63 @@ document.querySelector('.go-top-container').addEventListener('click', () => {
 // MENU
 
 window.onload = function(){
-    const headerScroll = document.querySelector('.menu-header')
-    let scrollPos = 0
+    const headerScroll = document.querySelector('.menu-header');
+    let scrollPos = 0;
     
     window.addEventListener('scroll', function(){
         
         if((document.body.getBoundingClientRect()).top > scrollPos) {
-            headerScroll.style.top = "0"
+            headerScroll.style.top = "0";
         } else {
             if(document.documentElement.scrollTop > 150){ //Comentario en honor a agustin que es un genio con el js y me enseñó que se puede meter un if dentro de un else
-            headerScroll.style.top = "-100px"
+            headerScroll.style.top = "-100px";
             }
         }
-        scrollPos = (document.body.getBoundingClientRect()).top
+        scrollPos = (document.body.getBoundingClientRect()).top;
 
     });
-}
+};
 
 //Contador en index.html
 
 addEventListener('DOMContentLoaded', () => {
-    const contadores = document.querySelectorAll('.contador-boxes')
-    const velocidad = 225
+    const contadores = document.querySelectorAll('.contador-boxes');
+    const velocidad = 225;
 
     const animarContadores = () => {
         for (const contador of contadores){
             const actualizar_contador = () => {
                 let cantidad_maxima = +contador.dataset.cantidadTotal, valor_actual = +contador.innerText,
-                incremento = cantidad_maxima / velocidad
+                incremento = cantidad_maxima / velocidad;
 
                 if (valor_actual < cantidad_maxima) {
-                    contador.innerText = Math.ceil(valor_actual + incremento)
-                    setTimeout(actualizar_contador, 5)
+                    contador.innerText = Math.ceil(valor_actual + incremento);
+                    setTimeout(actualizar_contador, 5);
                 } else {
-                    contador.innerText = cantidad_maxima
+                    contador.innerText = cantidad_maxima;
                 }
-            }
-            actualizar_contador()
+            };
+            actualizar_contador();
         }
-    }
+    };
 
 
     const mostrarContadores = elementos => {
     elementos.forEach(elemento => {
         if(elemento.isIntersecting) {
-            elemento.target.classList.add('mostrar-contador')
-            elemento.target.classList.remove('ocultar-contador')
-            setTimeout(animarContadores, 300)
+            elemento.target.classList.add('mostrar-contador');
+            elemento.target.classList.remove('ocultar-contador');
+            setTimeout(animarContadores, 300);
         }
     });
-} 
+};
 
     const observer = new IntersectionObserver(mostrarContadores, {
         threshold: 0.75 // 0 - 1
-    })
+    });
 
-    const elementosHTML = document.querySelectorAll('.contador')
+    const elementosHTML = document.querySelectorAll('.contador');
     elementosHTML.forEach(elementoHTML => {
-        observer.observe(elementoHTML)
-    })
-})
+        observer.observe(elementoHTML);
+    });
+});
